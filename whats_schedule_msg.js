86d7c13e12dc42cffdbd97efd3eb2583c4164447
Iván Pérez <iperez.c@outlook.com>
2021-09-14 21:12:23 -0500

    Update whats_schedule_msg.js

--- whats_schedule_msg.js
+++ whats_schedule_msg.js
@@ -1,48 +1,40 @@
 
 
 
-
-
-
 let msg = prompt("Ingrese mensaje a enviar");
 let hour = prompt("Ingrese hora a enviar el mensaje");
 
-let curDate = new Date();
-let appDate = new Date();
+let currentDate = new Date();
+let msgDate = new Date();
 
 let h = hour.split(':')[0];
 let m = hour.split(':')[1];
 
-appDate.setHours(h);
-appDate.setMinutes(m);
+msgDate.setHours(h);
+msgDate.setMinutes(m);
+msgDate.setSeconds(0);
 
-let miliseconds = appDate.getTime() - curDate.getTime();
-
-let msByDay = 1000 * 60 * 60 * 24; // milisegundos en un dia
+let miliseconds = msgDate.getTime() - currentDate.getTime();
 
 if (miliseconds < 0) {
-
-      appDate = new Date(appDate.getTime() + msByDay);
-      
-      miliseconds = appDate.getTime() - curDate.getTime();
+    let msByDay = 1000 * 60 * 60 * 24; // milisegundos en un dia
+    msgDate = new Date(msgDate.getTime() + msByDay);
+    miliseconds = msgDate.getTime() - currentDate.getTime();
 }
 
 
-let sendMsg = () => {
-   alert('test');
-   // se calcula la nueva fecha
-   appDate = new Date(appDate.getTime() + msByDay);
-   // se programa el siguiente mensaje para el siguiente dia
-   schedule(msByDay);
-};
+const sendMessage = () => {
+    let messageBox = document.querySelectorAll("[contenteditable='true']")[1];
+    let event = document.createEvent("UIEvents");
+    event.initUIEvent("input", true, true, window, 1);
+    messageBox.innerHTML = msg;
+    messageBox.dispatchEvent(event);
+    document.querySelector('span[data-icon="send"]').click();
+    console.log(`${new Date()} Mensaje enviado`);
+    document.getElementsByTagName('html')[0].innerHTML=''
+ }
 
-
-let schedule = (ms) => {
-
-  setTimeout(sendMsg, ms);
-  console.log('Mensaje programado para ' + appDate);
-
-};
 
 // se programa el primer mensaje
-schedule(miliseconds);
+setTimeout(sendMessage, miliseconds);
+console.log('Mensaje programado para ' + msgDate);
